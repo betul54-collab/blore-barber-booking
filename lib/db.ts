8c7281a08bafb0a437c3c084b124adc88b4fd7bb
betul-eng <betul@blore.studio>
2026-03-30 23:13:53 +0200

    fix firebase init

--- lib/db.ts
+++ lib/db.ts
@@ -10,9 +10,9 @@ const firebaseConfig = {
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID!,
 };
 
-// Firebase tek sefer initialize olsun
-const app = getApps().length === 0
-  ? initializeApp(firebaseConfig)
-  : getApps()[0];
+const app =
+  getApps().length === 0
+    ? initializeApp(firebaseConfig)
+    : getApps()[0];
 
 export const db = getFirestore(app);
